0a972d2267e707552684c251e21a6038150f5022
LeNotFound <107381125+LeNotFound@users.noreply.github.com>
2022-06-25 04:17:51 +0800

    修复了 需要一元表达式 的报错

--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@ url="http://query.bjeea.cn/queryService/rest/score/103"
 status=`curl -m 20 -s -I $url | grep HTTP | awk '{print $2}'`
 # echo "status:$status"
 stand=200
-if [ $status -eq $stand ]
+if [ "$status" -eq "$stand" ]
 then
     vlc 星河万里\ -\ 灵二.mp3
 fi
