20b00cb43d8f4e872e090213a667f5afde3c2f67
LeNotFound <107381125+LeNotFound@users.noreply.github.com>
2022-06-25 04:26:44 +0800

    循环发送请求

--- main.sh
+++ main.sh
@@ -3,7 +3,12 @@ url="http://query.bjeea.cn/queryService/rest/score/103"
 status=`curl -m 20 -s -I $url | grep HTTP | awk '{print $2}'`
 # echo "status:$status"
 stand=200
-if [ "$status" -eq "$stand" ]
-then
-    vlc 星河万里\ -\ 灵二.mp3
-fi
+while :
+do
+    status=`curl -m 20 -s -I $url | grep HTTP | awk '{print $2}'`
+    if [ "$status" -eq "$stand" ]
+    then
+        vlc 星河万里\ -\ 灵二.mp3
+        break
+    fi
+done
