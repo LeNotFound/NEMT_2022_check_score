3470c70fa0b05b535074cc437e3795f1c15e2fa8
LeNotFound <107381125+LeNotFound@users.noreply.github.com>
2022-06-25 04:35:58 +0800

    修改比较方式
    解决了 需要整数表达式 的报错

--- main.sh
+++ main.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 url="http://query.bjeea.cn/queryService/rest/score/103"
-status=`curl -m 20 -s -I $url | grep HTTP | awk '{print $2}'`
+# status=`curl -m 20 -s -I $url | grep HTTP | awk '{print $2}'`
 # echo "status:$status"
 stand=200
 while :
 do
     status=`curl -m 20 -s -I $url | grep HTTP | awk '{print $2}'`
-    if [ "$status" -eq "$stand" ]
+    if [ "$status" == "$stand" ]
     then
         vlc 星河万里\ -\ 灵二.mp3
         break
